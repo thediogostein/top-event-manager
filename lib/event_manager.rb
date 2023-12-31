67560f8b8f94fe53ddf61aefe94c85adee105bc3
Diogo Stein <98916171+thediogostein@users.noreply.github.com>
2023-12-31 16:01:03 -0300

    time targeting and week targeting

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -16,7 +16,6 @@ def clean_phone_number(phone_number)
   else
     "bad number"
   end
-
 end
 
 def legislators_by_zipcode(zip)
@@ -32,6 +31,18 @@ def legislators_by_zipcode(zip)
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
+end
+
+best_hours_arr = []
+
+def get_best_hours(date)
+  hour_and_minutes = date.split[1]
+  hour = hour_and_minutes.split(':')[0]
+  best_hours_arr.push(hour)
+end
+
+def get_best_days(date)
+
 end
 
 def save_thank_you_letter(id, form_letter)
@@ -55,14 +66,22 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+best_hours_arr = []
+best_days_arr = []
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  full_date_arr = row[:regdate].split
+
+  day =  Date.strptime(full_date_arr[0], '%m/%d/%Y')
+  best_days_arr.push(day.wday)
+
+  hour = full_date_arr[1].split(':')
+  best_hours_arr.push(hour[0])
 
   zipcode = clean_zipcode(row[:zipcode])
   phone = clean_phone_number(row[:homephone])
-
-  puts phone
 
   legislators = legislators_by_zipcode(zipcode)
 
@@ -71,3 +90,22 @@ contents.each do |row|
   save_thank_you_letter(id, form_letter)
 
 end
+
+best_hours_hash = best_hours_arr.reduce(Hash.new(0)) do |result, hour|
+  result[hour] += 1
+  result
+end
+
+best_days_hash = best_days_arr.reduce(Hash.new(0))  do |result, day|
+  result[day] += 1
+  result
+end
+
+best_hour = best_hours_hash.max_by{ |k,v| v}
+
+best_day = best_days_hash.max_by{ |k,v| v }
+
+best_day_name = Date::DAYNAMES[best_day[0].to_i]
+
+puts "best day is #{best_day_name}"
+puts "best hour is #{best_hour[0]}"
